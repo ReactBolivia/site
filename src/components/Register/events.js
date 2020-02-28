0bfaf9883ecc862fa2b1e607e806d43721426da1
GD DEV <gdavid.ptorrez@gmail.com>
2020-02-28 14:04:28 -0400

    IMP: Add files
    - Add qr file
    - Updated qr file in events.js

--- src/components/Register/events.js
+++ src/components/Register/events.js
@@ -3,7 +3,7 @@ const events = [
         "name": "React Loves Startups",
         "eventId": 19,
         "hasCost": true,
-        "qr": require('./qr-simple/reactober.png'),
+        "qr": require('./qr-simple/react-loves-startups-lp.jpeg'),
         "cost": 30,
         "discount": 0.33
     }
